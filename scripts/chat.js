99a6f5cc927ade2d59079ae4d970de652c30935b
Abhishek Prasad <34980034+abhpd@users.noreply.github.com>
2021-03-17 15:35:42 +0530

    rough design of two interfaces and creating and adding msgs to the list

---
+++ scripts/chat.js
@@ -0,0 +1,29 @@
+const sendButton = document.getElementById("send-button");
+const inputMsg = document.getElementById("input-msg");
+
+inputMsg.addEventListener("keyup", function (event) {
+    if (event.code === "Enter") {
+        event.preventDefault();
+        sendButton.click();
+    }
+});
+
+sendButton.addEventListener("click", () => {
+    const msg = document.getElementById("input-msg").value;
+    console.log(msg);
+    chatHistoryDivGenerator("abhi", msg);
+});
+
+const chatHistoryDivGenerator = (username, message) => {
+    const msg = document.createElement("P");
+    msg.innerHTML = username + " : " + message;
+
+    const chatHistory = document.createElement("DIV");
+    chatHistory.appendChild(msg);
+    chatHistory.classList.add("chat-history");
+
+    const chat = document.getElementById("chc");
+    chat.appendChild(chatHistory);
+
+    chatHistory.scrollIntoView();
+};
